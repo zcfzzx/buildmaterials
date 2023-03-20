b017c72b345f2741b9b4068079dd7b414d1b2562
dean <380214469@qq.com>
2023-03-20 20:47:29 +0800

    111

--- db/springbootq4h82.sql
+++ db/springbootq4h82.sql
@@ -2,7 +2,7 @@
 --
 -- Host: localhost    Database: springbootq4h82
 -- ------------------------------------------------------
--- Server version	5.7.31
+-- Server version1	5.7.31
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
